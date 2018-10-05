88325d731b6be1852e75a2a0ec89e42a6dfa2db4
talaviya bhavdip <talaviya.bhavdip@outlook.com>
2018-10-05 21:34:33 +0530

    Submit dynamic servay form

---
+++ Repo_Pattern_MVC/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Repo_Pattern_MVC.Utility;
+using Repo_Pattern_MVC_Model;
+using Repo_Pattern_MVC_Model.Model;
+using Repo_pattern_MVC_Repository.Service;
+using Repo_pattern_MVC_Repository.ServiceContract;
+
+namespace Repo_Pattern_MVC.Controllers
+{
+	public class HomeController : Controller
+	{
+		IHome_Repository _Home_Repository = new Home_Repository(new Repo_Pattern_MVCEntities());
+		public ActionResult Index()
+		{
+		ViewBag.FormnameList=	_Home_Repository.GetFormName();
+			return View();
+		}
+		
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+		[ValidateInput(false)]
+		public JsonResult SaveForm(int UserId, string UserName, string Form_Data)
+		{
+			var result = new jsonMessage();
+			try
+			{
+				Form_Data_Master _modal = new Form_Data_Master();
+				_modal.UserId = UserId;
+				_modal.UserName = UserName;
+				_modal.Form_Data = Form_Data;
+
+				int Issave = _Home_Repository.SaveForm(_modal);
+				if (Issave == 1)
+				{
+					result.Message = "Form has beed submited successfully.";
+					result.Status = true;
+				}
+
+			}
+			catch (Exception ex)
+			{
+				//ErrorLogers.ErrorLog(ex);
+				result.Message = ex.ToString();
+				result.Status = false;
+			}
+			return Json(result, JsonRequestBehavior.AllowGet);
+		}
+
+	}
+}
---
+++ Repo_pattern_MVC_Repository/ServiceContract/Home_Repository.cs
@@ -0,0 +1,96 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repo_Pattern_MVC_Model;
+using Repo_Pattern_MVC_Model.Model;
+using Repo_pattern_MVC_Repository.Service;
+
+namespace Repo_pattern_MVC_Repository.ServiceContract
+{
+
+	public class Home_Repository : IHome_Repository
+	{
+		private Repo_Pattern_MVCEntities context;
+		public Home_Repository(Repo_Pattern_MVCEntities _context)
+		{
+			this.context = _context;
+		}
+		//start your method here...
+		
+		public int SaveDynamicForm(Form_Master _modal)
+		{
+
+			context.Form_Master.Add(_modal);
+			context.SaveChanges();
+			return 1;
+		}
+
+		public List<Form_Master> GetFormName()
+		{
+			return context.Form_Master.ToList();
+
+		}
+		public List<Form_Data_Master> GetFormList()
+		{
+			return context.Form_Data_Master.ToList();
+
+		}
+		public List<Form_Master> GetFormNamebyID(int id)
+		{
+			return context.Form_Master.Where(t => t.id == id).ToList();
+
+		}
+		public List<Form_Data_Master> GetSubmitedFormDatabyID(int id)
+		{
+			return context.Form_Data_Master.Where(t => t.Id == id).ToList();
+
+		}
+		
+		public bool CheckFormnameExist(string FormName)
+		{
+			try
+			{
+				return context.Form_Master.Where(t => t.Form_Name == FormName).Any();
+				
+			}
+			catch (Exception Ex)
+			{
+				return false;
+				
+			}
+			
+
+		}
+
+		public int SaveForm(Form_Data_Master _modal)
+		{
+
+			context.Form_Data_Master.Add(_modal);
+			context.SaveChanges();
+			return 1;
+		}
+		//------------------------------------
+		private bool disposed = false;
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!this.disposed)
+			{
+				if (disposing)
+				{
+					context.Dispose();
+				}
+			}
+			this.disposed = true;
+		}
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+	}
+
+
+}
